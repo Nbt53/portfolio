38bfb9d78de7dc2528f1803111a1039fa8110b30
nbt53 <nbt853.nh@gmail.com>
2023-08-08 17:49:55 +0100

    doodle

--- public/js/index.js
+++ public/js/index.js
@@ -1,126 +1,139 @@
 ///////// selectors/////////////
 
-const canvas = document.querySelector("canvas")
-const container = document.querySelector(".pad-body")
-const selectBtn = document.querySelectorAll(".btn-select")
-const selectColor = document.querySelectorAll(".color")
+const canvas = document.querySelector("canvas");
+const container = document.querySelector(".pad-body");
+const selectBtn = document.querySelectorAll(".btn-select");
+const selectColor = document.querySelectorAll(".color");
 
 const ctx = canvas.getContext('2d');
-
-
 
 ////////////// options ////////////////////////
 let drawStyle = "draw";
 let isDrawing = false;
 ctx.fillStyle = 'black';
 
-
 /////variables//////
-let prevX, prevY;
+let lastX, lastY;
 
 //////////resize canvas///////////
 
 canvas.width = container.clientWidth; /* Set the canvas width to its computed width */
 canvas.height = container.clientHeight; /* Set the canvas height to its computed height */
 
-///////////////////get mouse pos//////////
-let offset = $("canvas").offset();
-let currentMousePos = { x: -1, y: -1 };
+///////////////////get touch pos//////////
+let currentTouchPos = { x: -1, y: -1 };
 
-$(".pad-canvas").on("mousemove touchstart", () => {
+canvas.addEventListener("mousemove", (e) => {
     const rect = canvas.getBoundingClientRect();
-    let touch = event.touches && event.touches.length ? event.touches[0] : null;
-    currentMousePos.x = event.clientX - rect.left;
-    currentMousePos.y = event.clientY - rect.top;
-})
+    currentTouchPos.x = e.clientX - rect.left;
+    currentTouchPos.y = e.clientY - rect.top;
+});
+
+canvas.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    const rect = canvas.getBoundingClientRect();
+    const touch = e.touches[0];
+    currentTouchPos.x = touch.clientX - rect.left;
+    currentTouchPos.y = touch.clientY - rect.top;
+});
 
 /// Select style////////////
 //style//
-const selectStyle = () => {
+const selectStyle = (event) => {
     drawStyle = event.target.id;
-}
+};
 
-for (i = 0; i < selectBtn.length; i++) {
-    selectBtn[i].addEventListener("click", selectStyle);
-}
-///color///
-const displayColor = () => {
-    ctx.fillStyle = event.target.id;
-    console.log('here')
-}
-
-for (i = 0; i < selectColor.length; i++) {
-    selectColor[i].addEventListener("click", displayColor);
-    selectColor[i].style.backgroundColor = selectColor[i].id;
-}
-
-
-////////////turn drawing on/off ////////////////////////
-$("canvas").on("mousedown touchstart", () => {
-    event.preventDefault();
-    isDrawing = true;
-    lastX = event.clientX - canvas.offsetLeft;
-    lastY = event.clientY - canvas.offsetTop;
+selectBtn.forEach((button) => {
+    button.addEventListener("click", selectStyle);
 });
 
-$("body").on("mouseup touchend", () => {
-    event.preventDefault();
+///color///
+const displayColor = (event) => {
+    ctx.fillStyle = event.target.id;
+};
+
+selectColor.forEach((color) => {
+    color.addEventListener("click", displayColor);
+    color.style.backgroundColor = color.id;
+});
+
+////////////turn drawing on/off ////////////////////////
+canvas.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    isDrawing = true;
+    lastX = currentTouchPos.x;
+    lastY = currentTouchPos.y;
+});
+
+canvas.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    isDrawing = true;
+    const rect = canvas.getBoundingClientRect();
+    const touch = e.touches[0];
+    lastX = touch.clientX - rect.left;
+    lastY = touch.clientY - rect.top;
+});
+
+canvas.addEventListener("mouseup", () => {
     isDrawing = false;
-    prevX = null;
-    prevY = null;
+    lastX = null;
+    lastY = null;
+});
+
+canvas.addEventListener("touchend", () => {
+    isDrawing = false;
+    lastX = null;
+    lastY = null;
 });
 
 ///////DRAW!!!////////
-addEventListener('mousemove' , () => {
-    if (isDrawing) {
-        if (drawStyle === 'draw') {
-            draw(currentMousePos.x, currentMousePos.y);
-        } else if (drawStyle === 'drawAnchored') {
-            drawAnchored(currentMousePos.x, currentMousePos.y, lastX, lastY);
-        }
-
-    }
-})
-
-addEventListener('touchmove', (e) => {
-    if (isDrawing) {
-        if (drawStyle === 'draw') {
-            draw(currentMousePos.x, currentMousePos.y);
-        } else if (drawStyle === 'drawAnchored') {
-            drawAnchored(currentMousePos.x, currentMousePos.y, lastX, lastY);
-        }
-
-    }
-})
-
-///////////free Draw function/////////////////
 const draw = (x, y) => {
-    event.preventDefault();
     ctx.strokeStyle = ctx.fillStyle;
-    if (prevX != null && prevY != null) {
+    if (lastX != null && lastY != null) {
         ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
+        ctx.moveTo(lastX, lastY);
         ctx.lineTo(x, y);
         ctx.stroke();
+        ctx.closePath();
     }
-    prevX = x;
-    prevY = y;
+    lastX = x;
+    lastY = y;
 };
 
-//////////////////////weird pattern///////////
+canvas.addEventListener('mousemove', () => {
+    if (isDrawing) {
+        if (drawStyle === 'draw') {
+            draw(currentTouchPos.x, currentTouchPos.y);
+        } else if (drawStyle === 'drawAnchored') {
+            drawAnchored(currentTouchPos.x, currentTouchPos.y, lastX, lastY);
+        }
+    }
+});
+
+canvas.addEventListener('touchmove', (e) => {
+    if (isDrawing) {
+        if (drawStyle === 'draw') {
+            draw(currentTouchPos.x, currentTouchPos.y);
+        } else if (drawStyle === 'drawAnchored') {
+            drawAnchored(currentTouchPos.x, currentTouchPos.y, lastX, lastY);
+        }
+    }
+});
+
+///////////free Draw function/////////////////
 const drawAnchored = (x1, y1, x2, y2) => {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
-}
-
-///////////rectangle/////////
+    ctx.closePath();
+};
 
 /////////////////clear canvas////////////////
 const clearCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-$(".btn-clear").click(() => {
+};
+
+document.querySelector(".btn-clear").addEventListener("click", () => {
     clearCanvas();
-})
+});
