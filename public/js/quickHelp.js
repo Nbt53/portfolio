83129486e2706b1d8ccc46d762e9dcf1c0be8d63
nbt53 <nbt853.nh@gmail.com>
2023-08-14 09:39:03 +0100

    removed design

--- public/js/quickHelp.js
+++ public/js/quickHelp.js
@@ -5,15 +5,15 @@ let isOpen = false;
 button.addEventListener('click', () => {
 
     if (isOpen === false) {
-        form.style.width = '100%'
-        form.style.height = '100%'
-        form.style.padding = '5rem'
-        isOpen = true
+        form.style.width = '100%';   // Expand width to 100%
+        form.style.height = '80rem';  // Allow content to determine height  
+        form.style.padding = '5rem';
+        isOpen = true;
 
     } else {
-        form.style.width == '0rem'
-        form.style.height = '0rem'
-        form.style.padding = '0rem'
-        isOpen = false
+        form.style.width = '0';      // Shrink width to 0
+        form.style.height = '0';      
+        form.style.padding = '0rem';
+        isOpen = false;
     }
-})
+});
