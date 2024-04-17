8ad4ce6e2b7896ae2fbd3484c869fec72da809fc
nbt53 <nbt853.nh@gmail.com>
2024-04-17 18:06:03 +0100

    edited nav bar and a few tweeks

--- public/js/nav.js
+++ public/js/nav.js
@@ -6,41 +6,40 @@ const burgerLine2 = document.querySelector('.nav__hamburger-line-2')
 const burgerLine3 = document.querySelector('.nav__hamburger-line-3')
 
 ///check if icon is present then turn it off when scrolling
-if(document.querySelector('.nav__icon')){
+if (document.querySelector('.nav__icon')) {
     const navIcon = document.querySelector('.nav__icon')
-    window.addEventListener('scroll', function() {
-    
+    window.addEventListener('scroll', function () {
         const scrollPos = window.pageYOffset;
-    if(scrollPos > 0){
-    navIcon.style.display = 'none'
-    }else{
-        navIcon.style.display = 'block'
-    }
+        if (scrollPos > 0) {
+            navIcon.style.display = 'none'
+        } else {
+            navIcon.style.display = 'block'
+        }
     });
 }
 
 
 let expanded = 0
 
-const navExpand = () =>{
+const navExpand = () => {
     if (expanded === 0) {
         navBar.style.left = "0"
         expanded = 1
         burgerContainer.style.display = "block"
-      
+
     } else {
         navBar.style.left = "-70%"
         expanded = 0
         burgerContainer.style.display = "none"
     }
 
-    if(expanded === 0){
+    if (expanded === 0) {
         burgerLine1.style.top = "1.5rem"
         burgerLine1.style.transform = "rotate(0)"
         burgerLine2.style.display = "inline-block"
         burgerLine3.style.bottom = "1.5rem"
         burgerLine3.style.transform = "rotate(0)"
-    }else   if(expanded === 1){
+    } else if (expanded === 1) {
         burgerLine1.style.top = "unset"
         burgerLine1.style.transform = "rotate(45deg)"
         burgerLine2.style.display = "none"
